d1c22e3e43d072fc93a15654250d800660efb95b
elissegomez24 <elissegomez24@gmail.com>
2024-10-22 00:45:13 -0400

    updated MongoDB connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const Book = require('../models/Book');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+// Use the environment variable for MongoDB connection string
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+
+mongoose.connect(mongoURI, {
 })
     .then(() => {
         console.log('MongoDB connected!');
